76fc64b88361701d120956be0463d6bf478691de
Alba Casas Arzúa <acasas@OPEN3S.INT>
2022-05-20 15:34:38 +0200

    Create Nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,24 @@ import Button from "../Button";
 
 const Nav = () => {
   return (
-    <div className="h-[72px] p-3 w-full bg-darkBlue rounded-lg flex justify-end">
+    <div className="h-[72px] p-3 w-full bg-darkBlue rounded-lg flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <span className="font-bold text-lg text-white">6 suggestions</span>
+        <div>
+          <span className="text-sm text-white ">Sort by:</span>
+          <select
+            className="text-sm bg-transparent font-bold text-white border-none outline-0  "
+            name="select"
+          >
+            <option value="value1">Value 1</option>
+            <option value="value2" selected>
+              Value 2
+            </option>
+            <option value="value3">Value 3</option>
+          </select>
+        </div>
+      </div>
+
       <Button>Add feedback</Button>
     </div>
   );
